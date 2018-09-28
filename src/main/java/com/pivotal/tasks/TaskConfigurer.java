82956d6b02b2ce0ccf923166eec6659d1169b40b
kkester <kkester@pivotal.io>
2018-09-28 09:04:30 -0500

    Minor Config Fixes

--- src/main/java/com/pivotal/tasks/TaskConfigurer.java
+++ src/main/java/com/pivotal/tasks/TaskConfigurer.java
@@ -1,14 +1,11 @@
 package com.pivotal.tasks;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.task.configuration.DefaultTaskConfigurer;
-import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 public class TaskConfigurer extends DefaultTaskConfigurer {
 
-    @Autowired
     private DataSource dataSource;
 
     public TaskConfigurer(DataSource dataSource) {
